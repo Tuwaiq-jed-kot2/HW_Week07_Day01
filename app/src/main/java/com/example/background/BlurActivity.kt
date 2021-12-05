fefc7f41844b9b324f9abe6803067021a1ea41b4
ASUS <EshraqAlmalki>
2021-12-05 23:15:20 +0300

    finish, was fun!

--- app/src/main/java/com/example/background/BlurActivity.kt
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,10 +16,13 @@
 
 package com.example.background
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
 import com.example.background.databinding.ActivityBlurBinding
 
 class BlurActivity : AppCompatActivity() {
@@ -33,10 +36,51 @@ class BlurActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
+
         binding = ActivityBlurBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+            val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+            actionView.resolveActivity(packageManager)?.run {
+                startActivity(actionView)
+            }
+
+            }
+        }
+
+        binding.cancelButton.setOnClickListener { viewModel.cancelWork() }
 
         binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+    }
+
+    private fun workInfosObserver(): Observer<List<WorkInfo>>{
+        return Observer { listOfWorkInfo ->
+            if (listOfWorkInfo.isNullOrEmpty()){
+                return@Observer
+            }
+
+            val workInfo= listOfWorkInfo[0]
+
+            if (workInfo.state.isFinished){
+                showWorkFinished()
+
+
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
+
+            }else{
+                showWorkInProgress()
+            }
+        }
     }
 
     /**
--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -20,8 +20,13 @@ import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.*
+import com.example.background.workers.BlurWorker
+import com.example.background.workers.CleanupWorker
+import com.example.background.workers.SaveImageToFileWorker
 
 
 class BlurViewModel(application: Application) : ViewModel() {
@@ -29,14 +34,76 @@ class BlurViewModel(application: Application) : ViewModel() {
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
 
+    private val workManager = WorkManager.getInstance(application)
+    internal val outputWorkInfos: LiveData<List<WorkInfo>>
+
     init {
         imageUri = getImageUri(application.applicationContext)
+        outputWorkInfos = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
     }
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    internal fun applyBlur(blurLevel: Int) {}
+    internal fun applyBlur(blurLevel: Int) {
+
+//        var continuation = workManager
+//            .beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+
+        var continuation = workManager
+            .beginUniqueWork(
+                IMAGE_MANIPULATION_WORK_NAME,
+                ExistingWorkPolicy.REPLACE,
+                OneTimeWorkRequest.from(CleanupWorker::class.java)
+            )
+
+        for (i in 0 until blurLevel) {
+            val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+            if (i == 0) {
+                blurBuilder.setInputData(createInputDataForUri())
+            }
+
+            //val blurRequest = OneTimeWorkRequest.Builder(BlurWorker::class.java)
+            // .setInputData(createInputDataForUri())
+            // .build()
+
+            continuation = continuation.then(blurBuilder.build())
+        }
+
+        //  val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
+
+//        continuation = continuation.then(save)
+//
+//        continuation.enqueue()
+
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .build()
+
+
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .setConstraints(constraints)
+            .addTag(TAG_OUTPUT).build()
+        continuation = continuation.then(save)
+        continuation.enqueue()
+    }
+
+//        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>().build()
+//        continuation = continuation.then(save)
+//        continuation.enqueue()
+
+
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI , it.toString())
+        }
+
+        return builder.build()
+
+    }
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
@@ -72,5 +139,10 @@ class BlurViewModel(application: Application) : ViewModel() {
                 throw IllegalArgumentException("Unknown ViewModel class")
             }
         }
+    }
+
+
+    internal fun cancelWork() {
+        workManager.cancelUniqueWork(IMAGE_MANIPULATION_WORK_NAME)
     }
 }
